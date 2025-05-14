c45b1b0970db9891023a011f3542c3d0d7bb5bbe
livia #16055 <liviamoreirajbb@gmail.com>
2025-05-13 22:52:02 -0300

    Exercício 2 (Sem comentários)

--- exercicio1.java
+++ exercicio1.java
@@ -7,21 +7,21 @@ public class exercicio1 {
     public static void main(String[] args) {
 
 //Variáveis
-int numero_um, numero_dois;                                //Declaração das variáveis numero_um e numero_dois como inteiras.
+int numero_um, numero_dois;//Declaração das variáveis numero_um e numero_dois como inteiras.
 
 
 //Entrada do Usuário
-Scanner teclado= new Scanner(System.in);                  //Criação de um Scanner.
-System.out.println("Digite o primeiro número:");        //Escrever na tela a mensagem para o usuário.
-numero_um = teclado.nextInt();                            //Armazenar a entrada no usuário na variável numero_um.
-System.out.println("Digite o segundo número: ");        //Escrever na tela a mensagem para o usuário.
-numero_dois = teclado.nextInt();                          //Armazenar a entrada no usuário na variável numero_dois.
+Scanner teclado= new Scanner(System.in); //Criação de um Scanner.
+System.out.println("Digite o primeiro número:");//Escrever na tela a mensagem para o usuário.
+numero_um = teclado.nextInt();//Armazenar a entrada no usuário na variável numero_um.
+System.out.println("Digite o segundo número: ");//Escrever na tela a mensagem para o usuário.
+numero_dois = teclado.nextInt();//Armazenar a entrada no usuário na variável numero_dois.
 
 //Lógica
-    if (numero_um == numero_dois) {                                                     //Condição que verifica se a variável numero_um é igual a da numero_dois.
-        System.out.println("Os dois números digitados são iguais");                   //Escreve na tela a mensagem para o usuário.
+    if (numero_um == numero_dois) {//Condição que verifica se a variável numero_um é igual a da numero_dois.
+        System.out.println("Os dois números digitados são iguais");//Escreve na tela a mensagem para o usuário.
         }
-    else if  (numero_um > numero_dois) {                                                //Condição que verifica se a variável numero_um é maior que a numero_dois.
+    else if  (numero_um > numero_dois) {//Condição que verifica se a variável numero_um é maior que a numero_dois.
         System.out.println("O número "+ numero_um + " é maior que o número "+ numero_dois);//Escreve na tela a mensagem para o usuário.
         }
     else {
--- exercicio2.java
+++ exercicio2.java
@@ -1,3 +1,31 @@
+/* COMANDO
+Faça um programa que receba as duas notas de um aluno, calcule sua média, e que imprima a sua situação: >= 7 -> Aprovado < 7 -> Reprovado*/
+
+import java.util.Scanner;
+
 public class exercicio2 {
-    
+    public static void main(String[] args) {
+
+//Variáveis
+float nota_um, nota_dois, media;
+
+//Entradas do Usuário
+Scanner entrada = new Scanner (System.in);
+System.out.println("Digite a primeira nota: ");
+nota_um = entrada.nextFloat();
+System.out.println("Digite a segunda nota: ");
+nota_dois = entrada.nextFloat();
+
+//Lógica
+media = (nota_um+nota_dois)/2;
+
+if (media >= 7) {
+    System.out.println("Aprovado!");
+}
+else {
+    System.out.println("Reprovado!");
+}
+
+entrada.close();
+    }
 }
