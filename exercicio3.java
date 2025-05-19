a862011205faeafa822c617ea114c8e194a265c4
livia #16055 <liviamoreirajbb@gmail.com>
2025-05-19 18:47:47 -0300

    Exercício 3

---
+++ exercicio3.java
@@ -0,0 +1,44 @@
+/*COMANDO
+Faça um programa que receba 3 notas de um aluno, calcule e mostre uma mensagem de acordo com sua média:
+>= 0 e <3 --> Reprovado
+>= 3 e <7 --> Exame
+>= 7 e <=10 --> Aprovado 
+*/
+
+import java.util.Scanner;
+
+public class exercicio3 {
+        public static void main(String[] args) {
+
+//Variáveis
+float nota_um, nota_dois, nota_tres, media;
+
+//Entradas do usuário
+Scanner teclado = new Scanner (System.in);
+System.out.println("Digite a primeira nota:");
+nota_um = teclado.nextFloat();
+System.out.println("Digite a segunda nota:");
+nota_dois = teclado.nextFloat();
+System.out.println("Digite a terceira nota:");
+nota_tres = teclado.nextFloat();
+
+//Lógica
+media  = (nota_um + nota_dois + nota_tres)/3;
+
+    if (media < 0 || media > 10 ) {
+        System.out.println("ERRO:  A nota digitada é inválida");
+        }
+    else if (media < 3) {
+        System.out.println("Reprovado");
+        }
+    else if (media < 7) {
+        System.out.println("Fazer Exame");
+        }
+    else {
+        System.out.println("Aprovado");
+        }
+
+teclado.close();
+
+ }
+}
---
+++ exercicio4.java
@@ -0,0 +1,5 @@
+/*Dados três valores X,Y,Z, verificar se eles podem ser os comprimentos dos lados de um triângulo. Se eles não formarem um triângulo escrever uma mensagem. Considerar que o comprimento de cada lado de um triângulo é menor que a soma dos outros dois lados. */
+
+public class exercicio4 {
+    
+}
